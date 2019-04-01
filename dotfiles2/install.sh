f34210911bae4ff4495f4777de298c26525b810b
Fiahil <fiahil@gmail.com>
2019-04-01 13:55:08 +0200

    wip

--- dotfiles2/install.sh
+++ dotfiles2/install.sh
@@ -2,6 +2,23 @@
 
 function install () {
     echo "> Installing dotfiles"
+
+    export DOTDIR='$HOME/.dotfiles'
+    export REMOTE='git@github.com:fiahil/dotfiles2.git'
+
+    git init --bare "${DOTDIR}"
+
+    alias dotfiles="git --git-dir=${DOTDIR} --work-tree=${HOME}"
+
+    dotfiles config status.showUntrackedFiles no
+    dotfiles remote add origin "${REMOTE}"
+    dotfiles fetch
+    dotfiles reset origin/master
+
+    echo "> Installation completed !"
+    echo "> Files existing before:"
+
+    dotfiles status
 }
 
 install
