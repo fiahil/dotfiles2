8fcc25c86e43943fe01831101eb5664fa1add8c9
Quentin Leffray <Leffray.Quentin@bcg.com>
2019-04-09 21:33:28 +0200

    update install script

--- dotfiles2/install.sh
+++ dotfiles2/install.sh
@@ -4,7 +4,20 @@ set -euo pipefail
 
 function install () {
     echo "> Installing dotfiles"
-    curl -fsSL https://raw.githubusercontent.com/fiahil/dotfiles2/master/.zsh/functions/dotfiles | { cat ; echo "dotfiles init" } | zsh
+    curl -fsSL https://raw.githubusercontent.com/fiahil/dotfiles2/master/.zsh/functions/dotfiles \
+        | { cat ; echo "dotfiles init" } \
+        | zsh
+
+    if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
+        vim -E -s +PlugUpgrade +qa
+    else
+        curl -fsSLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
+    vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
+
+    reset -Q
+
     echo "> Installation completed !"
 }
 
