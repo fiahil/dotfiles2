0e1e7b59e0160d5ded3fa6ca68b25ff35ecb21ce
Quentin Leffray <fiahil@gmail.com>
2021-07-16 11:22:24 +0200

    backup

--- dotfiles2/backup.sh
+++ dotfiles2/backup.sh
@@ -6,12 +6,12 @@ echo "Starting backup"
 
 command -v restic > /dev/null 2>&1 || { echo >&2 "restic not installed. Aborting."; exit 1; }
 
-command -v op  > /dev/null 2>&1 && { 
+command -v op  > /dev/null 2>&1 && {
     eval $(op signin trofray)
 
     export B2_ACCOUNT_ID=$(op get item "Backblaze B2 Backups" --fields B2_ACCOUNT_ID --format CSV)
     export B2_ACCOUNT_KEY=$(op get item "Backblaze B2 Backups" --fields B2_ACCOUNT_KEY --format CSV)
-    
+
     export RESTIC_PASSWORD=$(op get item "Backblaze B2 Backups" --fields BACKUP_PASSWORD --format CSV)
 }
 
@@ -22,20 +22,41 @@ command -v op  > /dev/null 2>&1 && {
 export RESTIC_REPOSITORY="b2:fiahil-backups:$(hostname)"
 
 # check if repo already exist
-restic snapshots
+restic snapshots 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
     echo "> Repository not found for this machine"
     restic init
 fi
 
-restic --verbose backup        \
-    --exclude ".DS_Store"      \
-    --exclude "venv"           \
-    --exclude ".venv"          \
-    --exclude ".mypy_cache"    \
-    --exclude ".pytest_cache"  \
-    --exclude ".cache"         \
-    --exclude "target"         \
-    --exclude "data"           \
-    --exclude "node_modules"   \
-    --files-from "$HOME/dotfiles2/backup-files.txt"
+case $1 in
+    "status")
+        echo "> status"
+        restic snapshots
+        ;;
+    "run")
+        echo "> starting backups"
+
+        restic --verbose backup        \
+            --exclude ".DS_Store"      \
+            --exclude "venv"           \
+            --exclude ".venv"          \
+            --exclude ".mypy_cache"    \
+            --exclude ".pytest_cache"  \
+            --exclude ".cache"         \
+            --exclude "target"         \
+            --exclude "data"           \
+            --exclude "node_modules"   \
+            --files-from "$HOME/dotfiles2/backup-files.txt"
+        ;;
+    "cleanup")
+        echo "> starting cleanup"
+
+        restic forget                 \
+            --keep-last 6             \
+            --keep-monthly 6
+        ;;
+    *)
+        echo "> USAGE [status | run | cleanup]"
+        exit 1
+        ;;
+esac
